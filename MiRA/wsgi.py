22129efb4264e0c95cb210d6fb58958337ff8aa6
Steven Bradley <bradleysp@sraosha.csbc.vcu.edu>
2014-05-15 12:37:04 -0400

    make naming convention consistent

--- MiRA/wsgi.py
+++ MiRA/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for dataview project.
+WSGI config for MiRa project.
 
 This module contains the WSGI application used by Django's development server
 and any production WSGI deployments. It should expose a module-level variable
@@ -24,7 +24,7 @@ if path not in sys.path:
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 os.environ["DJANGO_SETTINGS_MODULE"] = "MiRA.settings"
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dataview.settings")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MiRA.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
